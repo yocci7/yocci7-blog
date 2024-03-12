9e72f87aa3815345999c895e29c6865ea02196b0
yocci_ii <hiitoy0117@gmail.com>
2024-03-13 03:09:21 +0900

    commit

--- app/article/[slug]/page.tsx
+++ app/article/[slug]/page.tsx
@@ -5,6 +5,7 @@ import matter from 'gray-matter';
 import { unified } from 'unified';
 import remarkParse from 'remark-parse';
 import remarkHtml from 'remark-html';
+import Link from 'next/link';
 import "@/public/Styles/Content.css";
 import { Metadata } from 'next';
 import { generateMetadataForArticle } from '@/app/components/elements/ArticleHead/ArticleHead';
@@ -44,16 +45,63 @@ export default async function ArticlePost({ params }: { params: { slug: string }
   const processedContent = await unified().use(remarkParse).use(remarkHtml).process(content);
   const contentHtml = processedContent.toString(); // 記事の本文をHTMLに変換
 
+  // 前後の記事へのリンク先を取得
+  const postsDirectory = path.join(process.cwd(), 'content');
+  const fileNames = fs.readdirSync(postsDirectory);
+  const currentPostIndex = fileNames.findIndex((fileName) => fileName.replace('.md', '') === slug);
+
+  // 記事を日付でソート
+  const sortedPosts = fileNames
+    .map((fileName) => {
+      const filePath = path.join(postsDirectory, fileName);
+      const fileContents = fs.readFileSync(filePath, 'utf8');
+      const { data } = matter(fileContents);
+      return { slug: fileName.replace('.md', ''), headline: data.headline, datePublished: data.datePublished };
+    })
+    .sort((a, b) => new Date(b.datePublished).getTime() - new Date(a.datePublished).getTime());
+
+  const currentIndex = sortedPosts.findIndex((post) => post.slug === slug);
+  const previousPostSlug = currentIndex < sortedPosts.length - 1 ? sortedPosts[currentIndex + 1].slug : null;
+  const nextPostSlug = currentIndex > 0 ? sortedPosts[currentIndex - 1].slug : null;
+
+  // 記事のヘッドラインを取得
+  const previousPostHeadline = previousPostSlug ? sortedPosts[currentIndex + 1].headline : null;
+  const nextPostHeadline = nextPostSlug ? sortedPosts[currentIndex - 1].headline : null;
+
   return (
     <>
       <div className='Contents'>
-        <h1 className='Title'>{headline}</h1>
-        <div className='Date'>
-          <p className='Date__Published'>{Published}</p>
-          <p className='Date__Updated'>{Updated}</p>
+        <h1 className='Contents__Title'>{headline}</h1>
+        <div className='Contents__Date'>
+          <p className='Contents__Date__Published'>{Published}</p>
+          <p className='Contents__Date__Updated'>{Updated}</p>
         </div>
-        <p className='Description'>{description}</p>
-        <div className='Contect' dangerouslySetInnerHTML={{ __html: contentHtml }}></div>
+        <p className='Contents__Description'>{description}</p>
+        <div className='Contents__Content' dangerouslySetInnerHTML={{ __html: contentHtml }}></div>
+        <div className='Contents__Link'>
+          {previousPostSlug && (
+            <Link href={`/article/${previousPostSlug}`} className='Contents__Link__Previous'>
+              前の記事 :
+              <p className='Contents__Link__Previous__headline'>
+                {previousPostHeadline.length > 53
+                  ? `${previousPostHeadline.slice(0, 53)}...`
+                  : previousPostHeadline
+                }
+              </p>
+            </Link>
+          )}
+          {nextPostSlug && (
+            <Link href={`/article/${nextPostSlug}`} className='Contents__Link__Next'>
+              次の記事 :
+              <p className='Contents__Link__Next__headline'>
+                {nextPostHeadline.length > 53
+                  ? `${nextPostHeadline.slice(0, 53)}...`
+                  : nextPostHeadline
+                }
+              </p>
+            </Link>
+          )}
+        </div>
       </div>
     </>
   );
